d747f5549a63cdebab7b70b58e337fdfbc425361
r00t254 <root254.mary@gmail.com>
2022-05-15 16:36:23 +0300

    add changes made

---
+++ blog.py
@@ -0,0 +1,52 @@
+from flask import Flask,render_template,flask
+from datetime import datetime
+from flask_sqlaichemy import SQLAIchemy
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash
+from datetime import date
+from webforms import loginForm,PostForm,UserForm
+from flask_login import UserMixin,Login_user
+from webforms import loginForm,PostForm,UserForm
+
+
+# create a Flask Instance
+app = Flask(__name__)
+# Add Database
+# Old SQLite DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite'
+# New MySQL DB
+# app,config[SQLALCHEMY_DATABASE_URI] = 'mysqlalchemy'
+
+# Secreate Key
+app.config['SECRETE_KEY'] ='my super secret key'
+db =SQLAIchemy(app)
+migrate = Migrate(app,db)
+# flask_login stuff
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_use(user_id):
+    return Users.query.get(int(user_id))
+
+# Create Search Function
+@app.route('/search',ethods=["POST"])
+def search():
+    pass
+
+# Create Login Page
+@app.route('/login', methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form)
+        if user:
+            # check the hash
+            if check_password_hash(user.check_password_hash):
+                Login_user(user)
+                flash("login Successful")
+                return redirect(url_for('dashboard'))
+            else:
+                flash("Wrong Password -Try Again")
+
